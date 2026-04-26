20070fc70aa6d3fe0aa453225d04abc8484aa0ed
Sharmada S <117641433+speckofstardust@users.noreply.github.com>
2026-04-26 09:12:41 +0530

    Update submission-4.py

--- Data Structures & Algorithms/three-integer-sum/submission-4.py
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -5,7 +5,7 @@ class Solution:
         nums = sorted(nums) #O(nlog(n))
         res = []
         for i in range(len(nums)):
-            if i != 0 and nums[i] == nums[i-1]:
+            if i != 0 and nums[i] == nums[i-1]: #to ensure no duplicate 1st value
                 continue
             j = i + 1
             k = len(nums) - 1
@@ -13,9 +13,9 @@ class Solution:
             while j<k:
                 if target == nums[j] + nums[k]:
                     res.append([nums[i], nums[j], nums[k]])
-                    while j<k and nums[j] == nums[j+1]:
+                    while j<k and nums[j] == nums[j+1]: #to skip duplicate 2nd value
                         j += 1
-                    while j<k and nums[k] == nums[k-1]:
+                    while j<k and nums[k] == nums[k-1]: #to skip duplicate 3rd value
                         k -= 1
                     j += 1
                     k -= 1
