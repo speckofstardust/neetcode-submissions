4b8b9dcaa0cdc21031307a571a4167367fc9a1f7
Sharmada S <117641433+speckofstardust@users.noreply.github.com>
2026-04-26 09:09:48 +0530

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[int]:
+        #if I sort nums, I can just skip if I've checked for that number already to eal with the duplicates
+        #target = -nums[i], 2Sum on the rest of the array for the target
+        nums = sorted(nums) #O(nlog(n))
+        res = []
+        for i in range(len(nums)):
+            if i != 0 and nums[i] == nums[i-1]:
+                continue
+            j = i + 1
+            k = len(nums) - 1
+            target = -nums[i]
+            while j<k:
+                if target == nums[j] + nums[k]:
+                    res.append([nums[i], nums[j], nums[k]])
+                    while j<k and nums[j] == nums[j+1]:
+                        j += 1
+                    while j<k and nums[k] == nums[k-1]:
+                        k -= 1
+                    j += 1
+                    k -= 1
+                elif target > nums[j] + nums[k]:
+                    j += 1
+                else:
+                    k -= 1
+        return res
